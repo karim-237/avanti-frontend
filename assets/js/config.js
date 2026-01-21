e4624c4882e0d2483eaec110b7293cded4a728db
karim-237 <karimduval20@gmail.com>
2026-01-21 11:46:10 +0100

    update

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 // assets/js/config.js
 window.API_BASE = window.location.hostname.includes('localhost')
-  ? 'http://https://avanti-backend-67wk.onrender.com/api'
+  ? 'https://avanti-backend-67wk.onrender.com/api'
   : 'https://avanti-api.onrender.com/api';
